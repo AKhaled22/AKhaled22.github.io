6f83e95a433bca93f2bf1a9e00dfc08df91393aa
AKhaled22 <84202515+AKhaled22@users.noreply.github.com>
2023-07-12 04:10:06 +0300

    Add files via upload

---
+++ HICModification.js
@@ -0,0 +1,98 @@
+// HIC 
+///////////////////////////////
+const bodyWeight = document.querySelector(".bWeight")
+const height = document.querySelector(".heightt")
+const aptt = document.querySelector(".aptt")
+const currRateH = document.querySelector(".currRateH")
+const calcModButt = document.querySelector(".HICModificationbutton")
+
+const gender = document.querySelector("#gender")
+
+
+
+function usedBodyWeight() {
+    let BMI = bodyWeight.value / (height.value / 100)
+    let IBW = (gender.value == "M") ? (50 + (2.3 * ((height.value / 2.5) - 60))) : (45 + (2.3 * ((height.value / 2.5) - 60)))
+    if (BMI < 40) {
+        return bodyWeight.value
+
+    } else {
+        return ((0.4 * (bodyWeight.value - IBW)) + IBW)
+    }
+}
+
+
+
+
+
+
+function HICModificationEvent() {
+    let extraBolus = 0
+    let changingRate = 0
+    let usedBW = usedBodyWeight()
+    const result1 = document.querySelectorAll(".resultsHIC").item(0)
+    const result2 = document.querySelectorAll(".resultsHIC").item(1)
+    let x = 0
+    if (!bodyWeight.value) {
+        bodyWeight.style.borderColor = "red"
+        x = 1
+    } else {
+        bodyWeight.style.borderColor = ""
+    }
+    if (!height.value) {
+        height.style.borderColor = "red"
+        x = 1
+    } else {
+        height.style.borderColor = ""
+    }
+    if (!aptt.value) {
+        aptt.style.borderColor = "red"
+        x = 1
+    } else {
+        aptt.style.borderColor = ""
+
+    }
+    if (!currRateH.value) {
+        currRateH.style.borderColor = "red"
+        x = 1
+    } else {
+        currRateH.style.borderColor = ""
+    }
+    if (x == 1) {
+
+        result1.textContent = ""
+        result2.textContent = ""
+        return;
+    }
+    if (aptt.value < 35) {
+        extraBolus = 80 * usedBW / 100
+        changingRate = Math.round(4 * usedBW / 100 + currRateH.value)
+
+    } else if (aptt.value < 45) {
+        extraBolus = 40 * usedBW / 100
+        changingRate = Math.round(4 * usedBW / 100 + currRateH.value)
+
+    } else if (aptt.value < 70) {
+        changingRate = "No Change"
+
+    } else if (aptt.value < 90) {
+        changingRate = Math.round(currRateH.value - 2 * usedBW / 100)
+
+    } else {
+
+        changingRate = "Stop infusion for 1 hour then resume by " + Math.round(currRateH.value - 3 * usedBW / 100)
+
+    }
+
+    result1.textContent = "Extra Bolus = " + Math.round(extraBolus)
+    result1.classList.add("resultsHIC")
+    result2.textContent = "Changing Rate = " + (changingRate)
+    result2.classList.add("resultsHIC")
+
+}
+
+
+///////////////////////////////
+calcModButt.addEventListener("click", HICModificationEvent)
+
+///////////////////////////////
---
+++ IICMaintenance.js
@@ -0,0 +1,205 @@
+//IIC Page
+///////////////////////////////
+// const currCBGInit = document.querySelector("#currCBGInit")
+const prevCBGMaint =  document.querySelector("#prevCBGMaint")
+const currCBGMaint =  document.querySelector("#currCBGMaint")
+const currRateMaint =  document.querySelector("#currRateMaint")
+// const initButton =  document.querySelector(".initbutton")
+const maintButton =  document.querySelector(".maintbutton")
+// const initbResult =  document.querySelector(".initbResult")
+// const initrResult =  document.querySelector(".initrResult")
+const resultsElement = document.querySelector(".resultsIIC")
+// let initBolus = 0
+// let initRate = 0
+
+function roundHalf(num) {
+    return Math.round(num*2)/2;
+}
+
+function calculateDelta(){
+    let currentRateMaint = currRateMaint.value
+    if(currentRateMaint<3){
+
+        return 0.5
+
+    } else if(currentRateMaint<6.5){
+
+        return 1
+
+    }else if(currentRateMaint<10){
+
+        return 1.5
+
+    }else if(currentRateMaint<15){
+
+        return 2
+
+    }else if(currentRateMaint<20){
+
+        return 3
+
+    }else{
+
+        return 4
+
+    }
+
+}
+
+// function initiationEvent(){
+//     let x = roundHalf(currCBGInit.value/100)
+//     if(!currCBGInit.value){
+//         currCBGInit.style.borderColor = "red"
+//         initbResult.textContent = ""
+//         initrResult.textContent = ""
+
+//     }else{
+//         currCBGInit.style.borderColor = ""
+//     }
+
+//     if(currCBGInit.value<180){
+//         initbResult.textContent = "NA"
+//         initrResult.textContent = "NA"
+
+//     }else if(currCBGInit.value<300){
+//         initrResult.textContent = x
+//         initbResult.textContent = "NA"
+        
+//     }else{
+//         initrResult.textContent = x
+//         initbResult.textContent = x
+
+//     }
+
+// } 
+
+function maintetnanceEvent(){
+    const delta = roundHalf(calculateDelta())
+    const currCBG = currCBGMaint.value
+    const prevCBG = prevCBGMaint.value
+    const currRate = currRateMaint.value
+    let x=0
+    if(!prevCBGMaint.value){
+        prevCBGMaint.style.borderColor = "red"
+        x=1
+    }else{
+        prevCBGMaint.style.borderColor = ""
+    }
+    if(!currCBGMaint.value){
+        currCBGMaint.style.borderColor = "red"
+        x=1
+    }else{
+        currCBGMaint.style.borderColor = ""
+    }
+    if(!currRateMaint.value){
+        currRateMaint.style.borderColor = "red"
+        x=1
+    }else{
+        currRateMaint.style.borderColor = ""
+    }
+    if(x==1){
+        resultsElement.textContent = ""
+        return
+    }
+
+    let differenceCBG = currCBG - prevCBG 
+    let y=0
+    if(currCBG<100){
+        resultsElement.textContent = "DC insulin and give 100 mL D10, recheck every 15 minutes"
+
+    }else
+    if(currCBG>=100 && currCBG<=139){
+        if(differenceCBG>0){
+            resultsElement.textContent = "No Change"
+
+        }else if(differenceCBG<=0 && differenceCBG>=-20){
+            y=(Number(currRate) - delta)
+
+            resultsElement.textContent = "Decrease rate to " + (y) + " ml/hr"
+
+        }else{
+            resultsElement.textContent = "DC insulin -> CBG/30 min -> when CBG > 140 mg/dL -> restart at rate =  " + (roundHalf(0.75*currRate)) + " ml/hr"
+
+        }
+
+    }else if(currCBG==140){
+        if(differenceCBG>40){
+            y=(Number(currRate)+ delta)
+            resultsElement.textContent = "Increase rate to " +y + " ml/hr"
+        }else if(differenceCBG>=-20){
+            resultsElement.textContent = "No Change"
+            
+
+        }else if(differenceCBG>=-40){
+            y=(Number(currRate) - delta)
+            resultsElement.textContent = "Decrease rate to " + y + " ml/hr"
+
+
+        }else{
+           y= (Number(currRate)-2*delta)
+            resultsElement.textContent = "Hold infusion for 30 minutes then decrease rate to " + y + " ml/hr"
+
+
+        }
+        
+    }else if(currCBG<=200){
+
+        if(differenceCBG>60){
+            y=(Number(currRate) + 2*delta)
+            resultsElement.textContent = "Increase rate to " + y + " ml/hr"
+
+        }else if(differenceCBG>=0){
+            y=(Number(currRate)+ delta)
+            resultsElement.textContent = "Increase rate to " +y + " ml/hr"
+            
+
+        }else if(differenceCBG>=-40){
+            resultsElement.textContent = "No Change"
+
+
+        }else if(differenceCBG>=-60){
+            y= (Number(currRate) - delta) 
+            resultsElement.textContent = "Decrease rate to " +y+ " ml/hr"
+
+
+        }else{
+            y= (Number(currRate) - delta) 
+            resultsElement.textContent = "Hold infusion for 30 minutes then decrease rate to " + y + " ml/hr"
+
+
+        }
+
+    }else{
+        if(differenceCBG>0){
+            y=(Number(currRate) + 2*delta)
+            resultsElement.textContent = "Increase rate to " + y + " ml/hr"
+
+        }else if(differenceCBG>=-20){
+            y=(Number(currRate)+ delta)
+            resultsElement.textContent = "Increase rate to " +y +" ml/hr"
+            
+
+        }else if(differenceCBG>=-60){
+            resultsElement.textContent = "No Change"
+
+
+        }else if(differenceCBG>=-80){
+            y=(Number(currRate) - delta)
+            resultsElement.textContent = "Decrease rate to "+ y +" ml/hr"
+
+
+        }else{
+            y=(Number(currRate) - 2*delta)
+            resultsElement.textContent = "Hold infusion for 30 minutes then decrease rate to " + y + " ml/hr"
+
+
+        }
+
+    }
+
+
+}
+
+
+// initButton.addEventListener("click" , initiationEvent)
+maintButton.addEventListener("click" , maintetnanceEvent)
