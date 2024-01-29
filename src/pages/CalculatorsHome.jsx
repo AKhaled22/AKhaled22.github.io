9a1814a0cacdea9974decd18f306c925d80f6844
AKhaled22 <ahmedkhaled1029@gmail.com>
2024-01-29 21:42:02 +0200

    .

--- src/pages/CalculatorsHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CalculatorsHome = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default CalculatorsHome
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export { default as CalculatorsHome } from "./CalculatorsHome"
-export { default as HICInitialization } from "./HICInitialization"
-export { default as HICModification } from "./HICModification"
-export { default as IICInitiation } from "./IICInitiation"
-export { default as IICMaintenance } from "./IICMaintenance"
-export { default as DKAInitiation } from "./DKAInitiation"
-export { default as DKAModification } from "./DKAModification"
-
-
